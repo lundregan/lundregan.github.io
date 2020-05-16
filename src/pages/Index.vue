7b6aeaca04df47f3f47236160c84e10b0879b9a7
Ethan Lundregan <lundregan.ethan@gmail.com>
2020-05-16 13:10:30 +0100

    Basic content added

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,16 +4,25 @@
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <g-image alt="Example image" src="~/favicon.png" width="135" />
 
-    <h1>Hello, world!</h1>
+    <section id='intro-section'>
+      <h1 id='intro-name'>Ethan Lundregan</h1>
+      <h2 id='intro-position'>Junior Developer</h2>
+      
+      <p>
+        Ethan Lundregan Is My Name<br />
+        Programming Is My Game<br />
+        Cheesy Lines Arn't Lame<br />
+        Computing Is My Dame
+      </p>
 
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
+      <a href="https://github.com/lundregan" target="_blank" rel='noopener'>
+        <button id='view-projects-button'>View My Projects on Github</button>
+      </a>
+    </section>
 
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
+    <section id='projects-section'>
+      <!-- List of my porjects will go here -->
+    </section>
 
   </Layout>
 </template>
@@ -21,13 +30,53 @@
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: 'Lundregan'
   }
 }
 </script>
 
 <style>
+#intro-section {
+  text-align: center;
+}
+
+#intro-name {
+  margin: 0;
+
+  font-size: 1.8em
+}
+
+#intro-position {
+  margin-top: 0;
+
+  font-size: 1.2em;
+}
+
+#view-projects-button {
+  margin-top: 20px;
+
+  padding: 10px;
+
+  background: grey;
+
+  border: none;
+  border-radius: 10px;
+}
+#view-projects-button:hover {
+  animation: button-grow 2s;
+}
+
 .home-links a {
   margin-right: 1rem;
 }
+
+/* Animations  */
+
+@keyframes button-grow {
+  0% {transform: scale(1.0)}
+  50% {transform: scale(1.3)}
+  60% {transform: scale(1.4)}
+  100% {transform: scale(1.0)}
+}
+
 </style>
