47091494265ef55b9594e568ed7726de4e9dea10
lundregan <lundregan.ethan@gmail.com>
2020-05-20 15:18:51 +0000

    Added blog page (template ATM)

---
+++ src/pages/Blog.vue
@@ -0,0 +1,25 @@
+<template>
+  <Layout>
+    <h1>About</h1>
+    <p>
+      Template stuff
+    </p>
+    
+  </Layout>
+</template>
+
+<style>
+
+
+
+</style>
+
+<script>
+
+export default {
+  metaInfo: {
+    title: 'About us'
+  }
+}
+
+</script>
