1ed33236b5dbafd52037055914c8dab3138c2224
lundregan <lundregan.ethan@gmail.com>
2020-05-20 12:49:24 +0000

    Styling Contact form, status and contact sections

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,29 +1,37 @@
 <template>
   <Layout>
-    <h1>Status</h1>
-    <p v-if='!about.employed'> {{ about.work_status }} </p>
+    <section id='status-section'>
+      <div id='status-work'>
+        <h1>Status</h1>
+        <p v-if='!about.employed'> {{ about.work_status }} </p>
+      </div>
 
-    <p> Current Work</p>
-    <ul>
-      <li v-for='item in about.learning_status' :key='item'> {{ item }} </li>
-    </ul>
+      <div id='status-learning'>
+        <h1>Current Work</h1>
+        <p v-for='item in about.learning_status' :key='item'> {{ item }} </p>
+      </div>
+    </section>
 
-
-    <h1> Contact me </h1>
-    <form name="contact" method="POST" data-netlify="true">
-      <p>
-        <label>Your Name: <input type="text" name="name" /></label>   
-      </p>
-      <p>
-        <label>Your Email: <input type="email" name="email" /></label>
-      </p>
-      <p>
-        <label>Message: <textarea name="message"></textarea></label>
-      </p>
-      <p>
-        <button id='submitForm' type="submit">Send</button>
-      </p>
-    </form>
+    <section id='contact-section'>
+      <h1> Contact me </h1>
+      <form name="contact" method="POST" data-netlify="true">
+        <p>
+          <label>Your Name:</label><br />
+          <input type="text" name="name" />  
+        </p>
+        <p>
+          <label>Your Email:</label><br />
+          <input type="email" name="email" />
+        </p>
+        <p>
+          <label>Message:</label><br />
+          <textarea name="message"></textarea>
+        </p>
+        <p>
+          <button id='submitForm' type="submit">Send</button>
+        </p>
+      </form>
+    </section>
   </Layout>
 </template>
 
@@ -46,11 +54,40 @@ export default {
 
 <style>
 
+#status-section, #contact-section {
+  margin: 0 auto;
+
+  padding-top: 100px;
+
+  text-align: center;
+}
+
+#status-section {
+  max-width: 600px;
+}
+
+#contact-section {
+  max-width: 300px;
+}
+
+#status-learning {
+  margin-top: 6vh;
+}
+
 #submitForm {
   color: black;
 }
 
-form input {
+input, textarea {
+  margin: 5px 0 10px 0;
+
+  padding: 5px;
+ 
+  width: 100%;
+}
+
+textarea{
+  height: 10vh;
 }
 
 </style>
