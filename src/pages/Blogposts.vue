d66b7f926526a209a0bff6efaf5f31f05f56840b
lundregan <lundregan.ethan@gmail.com>
2020-05-21 15:52:09 +0000

    Created blogposts to see all the posted blog posts which are pulled in from markdown files

---
+++ src/pages/Blogposts.vue
@@ -0,0 +1,39 @@
+<template>
+  <Layout>
+    <h1>Blog stuff Testing</h1>
+
+    <div v-for='edge in $page.blog.edges' :key='edge.node.id'>
+      
+      <g-link :to='edge.node.path'> {{ edge.node.title }} </g-link>
+      
+      {{ edge.node.title }}
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+  query Documentation {
+    blog: allDocumentation {
+      edges {
+        node {
+          title,
+          path
+        }
+      }
+    }
+  }
+</page-query>
+
+<script>
+
+export default {
+  data(){
+    return {
+    }
+  },
+  
+  metaInfo: {
+    title: 'BlogPosts'
+  }
+}
+</script>
