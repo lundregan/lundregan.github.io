1aa4d575e46976a6a972c61d42c8470ab566c3d9
lundregan <lundregan.ethan@gmail.com>
2020-05-21 15:18:47 +0000

    Added blog template

---
+++ src/templates/Blog.vue
@@ -0,0 +1,27 @@
+<template>
+  <Layout>
+      <h1> This is blog </h1>
+
+      <VueRemarkContent />
+  </Layout>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<script> 
+
+
+
+</script>
+
+<style>
+
+
+
+</style>
