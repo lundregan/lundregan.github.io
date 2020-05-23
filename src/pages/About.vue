f505ee8858ccb7ead0d231cfebd4f70252b642f3
Ethan Lundregan <lundregan.ethan@gmail.com>
2020-05-23 21:38:42 +0100

    Split skills in about into catogories

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,18 +1,27 @@
 <template>
   <Layout>
-    <h1>About</h1>
-    <p>
-      I am a <strong>junior developer</strong>, who is itching to get into the industry and show off my passion and talent for web development.
-    </p>
-    <p>
-      Currently I am learning more about the world of <strong>Vue</strong>, and having a blast doing so.
-    </p>
+    <section class="about-section">
+      <h1>About</h1>
+      <p>
+        I am a <strong>Junior Developer</strong>, who is itching to get into the industry and show off my passion and talent for web development.
+      </p>
+      <p>
+        Currently I am learning more about the world of <strong>Vue</strong>, and having a blast doing so.
+      </p>
+    </section>
 
-    <h2>Skills</h2>
-    
-    <ul>
-      <li v-for="skill in about.skills" :key='skill'>{{skill}}</li>      
-    </ul>
+    <section class="skill-section">
+      <h1>Skills</h1>
+      <div class="skills">
+        <div class='skill-box' v-for='section in about.allSkills' :key='section'>
+          <h1 class="skill-section-title">{{section.title}}</h1>
+
+          <ul class="frontend-list">
+            <li v-for='skill in section.skills' :key='skill'> {{ skill }} </li>
+          </ul>
+        </div>
+      </div>
+    </section>
   </Layout>
 </template>
 
@@ -31,3 +40,41 @@ export default {
   }
 }
 </script>
+
+<style>
+
+section h1 {
+  text-align: center;
+}
+
+.skills {
+  display: flex;
+  justify-content: space-evenly;
+}
+
+.skill-box {
+  margin: 20px;
+  padding: 10px 20px;
+
+  min-width: 15vw;
+
+  text-align: center;
+
+  border-radius: 10px;
+
+  background: #212121;
+}
+
+.skill-section-title {
+  border-bottom: 2px #303030 solid;
+}
+
+ul {
+  padding: 0;
+}
+
+li {
+  list-style-type: none;
+}
+
+</style>
