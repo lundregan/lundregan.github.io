3ec79c0abe68131c2e2e8b32fe9a438ca17a58f9
lundregan <lundregan.ethan@gmail.com>
2020-10-28 23:03:33 +0000

    added augmented-ui

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: ['@/node_modules/augmented-ui/augmented-ui.min.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
