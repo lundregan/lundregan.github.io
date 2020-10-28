b038c6c97e7412513d752cd6c4de0a3ee3aa0e7d
lundregan <lundregan.ethan@gmail.com>
2020-10-28 16:39:56 +0000

    basic website layout and navbar

--- pages/index.vue
+++ pages/index.vue
@@ -2,25 +2,8 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">portfolio</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <h1 class="title">Lundregan</h1>
+      <h2 class="subtitle">Ethan Lundregan, Another Awesome Web Developer</h2>
     </div>
   </div>
 </template>
