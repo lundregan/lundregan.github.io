b2cb03cb6429bce85eee2e33ec45b59a23c2e4c5
lundregan <lundregan.ethan@gmail.com>
2020-10-29 21:20:49 +0000

    social icons grow on hover

--- components/SocialSideBar.vue
+++ components/SocialSideBar.vue
@@ -1,21 +1,17 @@
 <template>
-  <div
-    class="social-side-bar h-32"
-    data-augmented-ui="border tr-2-clip-x br-2-clip-x"
-    --aug-border-all="1"
-  >
+  <div class="social-side-bar h-32" data-augmented-ui="tr-2-clip-x br-2-clip-x">
     <div
       class="social-links flex flex-col my-4 py-16 align-center w-full h-full items-center justify-center"
     >
       <a
-        class="self-center hover:text-purple-500"
+        class="self-center hover:text-purple-500 grow"
         target="_blank"
         href="https://github.com/lundregan"
       >
         <fa class="fa-2x" :icon="['fab', 'github']" />
       </a>
       <a
-        class="self-center mt-4 hover:text-blue-500"
+        class="self-center mt-4 hover:text-blue-500 grow"
         target="_blank"
         href="https://twitter.com/lundregan1"
       >
@@ -33,8 +29,13 @@ export default {}
 .social-side-bar
   width: 3.5rem
   --aug-border-all: 1px
-  --aug-border-bg: #fcee0a
   background: #fcee0a
   position: sticky
   top: 48px
+
+.grow
+  transition: all .2s ease-in-out
+
+.grow:hover
+  transform: scale(1.25)
 </style>
