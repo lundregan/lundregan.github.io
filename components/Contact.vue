a7892cf8c4d82bcc9982a2688a82bc5bdf3d157f
lundregan <lundregan.ethan@gmail.com>
2020-11-04 14:50:09 +0000

    contact design

--- components/Contact.vue
+++ components/Contact.vue
@@ -7,37 +7,36 @@
       <p class="self-center">Bring your Vision to Life?</p>
     </div>
 
-    <form
-      action=""
-      class="flex flex-col bg-gray-900 px-32 py-16 h-1/3 mt-8"
-      data-augmented-ui="tl-clip br-clip border"
-    >
-      <div class="self-center w-full mb-8">
-        <label for="name">Name</label><br />
+    <form action="" class="form-contact flex flex-col px-32 py-16 h-1/3 mt-8">
+      <div class="self-center w-full mb-8 text-left">
+        <label for="name" class="pl-6">Name</label><br />
         <input
           type="text"
-          class="w-full rounded px-4 mt-2 p-2"
+          class="w-full rounded px-4 mt-2 p-2 form-input"
           placeholder="John Smith..."
+          data-augmented-ui="tl-clip br-clip border"
         />
       </div>
 
-      <div class="self-center w-full mb-8">
-        <label for="">E-mail</label><br />
+      <div class="self-center w-full mb-8 text-left">
+        <label for="" class="pl-6">E-mail</label><br />
         <input
           type="text"
-          class="w-full px-4 rounded mt-2 p-2"
+          class="w-full px-4 rounded mt-2 p-2 form-input"
           placeholder="example@test.com..."
+          data-augmented-ui="tl-clip br-clip border"
         />
       </div>
 
-      <div class="self-center">
-        <label for="msg">Message</label><br />
+      <div class="self-center text-left">
+        <label for="msg" class="pl-6">Message</label><br />
         <textarea
-          class="px-4 rounded mt-2"
+          class="px-4 rounded mt-2 form-input"
           name="msg"
           cols="50"
           rows="4"
           placeholder="Reason for contacting..."
+          data-augmented-ui="tl-clip br-clip border"
         ></textarea>
       </div>
 
@@ -59,8 +58,21 @@ export default {}
 </script>
 
 <style lang="sass">
+form
+  background: none
+
 label
-  color: white
+  color: yellow
+
+textarea
+  min-height: 40px
+
+.form-input
+  border-bottom: 4px solid transparent
+
+.form-input:focus
+  outline: none
+  border-bottom: 4px solid #fcee0a
 
 .btn-contact
   background: #fcee0a
