a3d02e4f4be837fc7a70801120e89b964005ac8b
lundregan <lundregan.ethan@gmail.com>
2020-11-06 21:30:40 +0000

    added hamburger menu for mobile

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,26 +1,85 @@
 <template>
-  <div class="navbar w-full" data-augmented-ui="br-2-clip-x bl-2-clip-x b-clip">
+  <div class="navs sticky top-0">
     <div
-      class="text-center flex flex-col text-2xl text-gray- sm:flex-row sm:justify-center md:justify-around lg:px-16 pb-6 sm:pt-2 md:font-semibold"
+      class="mobile-nav text-white text-center bg-yellow-400 sm:hidden"
+      data-augmented-ui="br-2-clip-x bl-2-clip-x b-clip"
     >
-      <nuxt-link to="/" class="link px-6 grow py-1">Home</nuxt-link>
-      <nuxt-link to="/#about" class="link px-6 grow py-1">About</nuxt-link>
-      <nuxt-link to="/#contact" class="link px-6 grow py-1">Contact</nuxt-link>
-      <nuxt-link to="#" class="link px-6 grow disabled py-1"
-        >Portfolio</nuxt-link
+      <fa
+        class="text-black m-4 text-4xl"
+        :icon="['fas', hamburgerMenuIcon]"
+        @click="openCloseHamburger()"
+      />
+      <div
+        v-if="hamburgerExpanded"
+        class="text-center flex flex-col text-2xl text-gray- sm:flex-row sm:justify-center md:justify-around lg:px-16 pb-6 sm:pt-2 md:font-semibold"
       >
-      <nuxt-link to="/blog" class="link px-6 grow py-1">Blog</nuxt-link>
+        <nuxt-link to="/" class="link px-6 grow py-1">Home</nuxt-link>
+        <nuxt-link to="/#about" class="link px-6 grow py-1">About</nuxt-link>
+        <nuxt-link to="/#contact" class="link px-6 grow py-1"
+          >Contact</nuxt-link
+        >
+        <nuxt-link to="#" class="link px-6 grow disabled py-1"
+          >Portfolio</nuxt-link
+        >
+        <nuxt-link to="/blog" class="link px-6 grow py-1">Blog</nuxt-link>
+      </div>
+    </div>
+    <div
+      class="navbar w-full invisible sm:visible"
+      data-augmented-ui="br-2-clip-x bl-2-clip-x b-clip"
+    >
+      <div
+        class="text-center flex flex-col text-2xl text-gray- sm:flex-row sm:justify-center md:justify-around lg:px-16 pb-6 sm:pt-2 md:font-semibold"
+      >
+        <nuxt-link to="/" class="link px-6 grow py-1">Home</nuxt-link>
+        <nuxt-link to="/#about" class="link px-6 grow py-1">About</nuxt-link>
+        <nuxt-link to="/#contact" class="link px-6 grow py-1"
+          >Contact</nuxt-link
+        >
+        <nuxt-link to="#" class="link px-6 grow disabled py-1"
+          >Portfolio</nuxt-link
+        >
+        <nuxt-link to="/blog" class="link px-6 grow py-1">Blog</nuxt-link>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      hamburgerExpanded: false,
+    }
+  },
+
+  computed: {
+    hamburgerMenuIcon() {
+      if (this.hamburgerExpanded) {
+        return 'times'
+      } else {
+        return 'hamburger'
+      }
+    },
+  },
+
+  methods: {
+    openCloseHamburger() {
+      this.hamburgerExpanded = !this.hamburgerExpanded
+    },
+  },
+}
 </script>
 
 <style lang="sass">
 *, *:before, *:after
     box-sizing: border-box
+
+.navs
+  z-index: 2
+
+.mobile-nav
+  background: #fcee0a
 
 .navbar
   --aug-border-all: 1px
