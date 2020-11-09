b7d98484f0a97762695d3e487d251945b197e1b2
lundregan <lundregan.ethan@gmail.com>
2020-11-09 15:35:30 +0000

    Redirect to home on 404

---
+++ pages/_.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>AHHHHHH WHAT WENT WRONG!!!!!</div>
+</template>
+
+<script>
+export default {
+  asyncData({ redirect }) {
+    return redirect('/')
+  },
+}
+</script>
+
+<style></style>
