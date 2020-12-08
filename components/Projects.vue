4be87dc8d7b53595d7bd9df21056a7cb4861b7b5
lundregan <lundregan.ethan@gmail.com>
2020-12-08 14:08:38 +0000

    new porject section

--- components/Projects.vue
+++ components/Projects.vue
@@ -1,10 +1,35 @@
 <template>
-  <div>Projects will go here!</div>
+  <div
+    class="w-full h-screen flex justify-center align-center items-center p-16 pt-64"
+  >
+    <div class="flex">
+      <div v-for="project in projects" :key="project.id" class="m-8 w-1/2">
+        <img class="project-image" :src="project.img" :alt="project.alt" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      projects: [
+        {
+          name: 'Desmos Corp',
+          img: '/project-images/desmoscorp.jpg',
+        },
+        {
+          name: 'Desmos Corp',
+          img: '/project-images/hexoggle.png',
+        },
+      ],
+    }
+  },
+}
 </script>
 
 <style lang="sass">
+.project-image
+  z-index: 1
 </style>
