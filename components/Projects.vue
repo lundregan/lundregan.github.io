812c1adb6f9abb2395b01cefe823ee4b2093487b
lundregan <lundregan.ethan@gmail.com>
2020-12-10 14:35:17 +0000

    Responsive imporvements

--- components/Projects.vue
+++ components/Projects.vue
@@ -4,7 +4,7 @@
       <div
         v-for="project in projects"
         :key="project.id"
-        class="m-8 w-1/4 relative shadow-md"
+        class="m-8 w-full md:w-1/2 lg:w-1/3 xl:w-1/4 relative shadow-md"
         @hover="project.hover == true"
       >
         <a :href="project.link" target="_blank">
@@ -23,12 +23,12 @@ export default {
         {
           name: 'Desmos Corp',
           img: '/project-images/desmoscorp.jpg',
-          link: 'https://lundregan.com'
+          link: 'https://lundregan.com',
         },
         {
           name: 'Hexoggle',
           img: '/project-images/hexoggle.png',
-          link: 'https://hexoggle.netlify.app/'
+          link: 'https://hexoggle.netlify.app/',
         },
       ],
     }
